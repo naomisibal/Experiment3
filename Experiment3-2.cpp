5c037f1dae72c69f368924c1dee5b02173342033
naomisibal <47804244+naomisibal@users.noreply.github.com>
2019-05-21 12:58:11 +0800

    Add files via upload

---
+++ Experiment3-2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <conio.h>
+#include<cmath>
+using namespace std;
+
+
+const int week = 7
+int main ()
+{
+	int temperature[province][week];
+    char province ='A'
+	
+	cout<<"Enter all temperatures for a week of Province A, Province B, Province C. ";
+	
+	While (i=0; province <= 'C')
+	{
+		for (int j=0; j<week; ++j)
+    
+        {
+        cout<< "Province " << province++; << ", Day " << j+1 << " : ";
+       
+		cin >> temperature[i][j];
+        }
+        
+	}
+	
+	While (province <= 'C')
+    {
+        for (int j=0; j<week; ++j)
+    
+        {
+        cout<< "Province " << province++; << ", Day " << j+1 << " : ";
+       
+        cin >> temperature[i][j];
+        }
+        
+    }	
+return 0;
+}
