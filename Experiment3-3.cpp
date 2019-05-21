6aca123de96917bbc46d534849b392bd4e318611
naomisibal <47804244+naomisibal@users.noreply.github.com>
2019-05-21 20:27:12 +0800

    Add files via upload

---
+++ Experiment3-3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <conio.h>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+
+    char a[25];
+    int num;
+    
+    cout << "Enter array: ";
+    
+    cin.getline(a, 25);
+    
+    num = strlen(a);
+    
+    for (int i = num; i >= 0; i--)
+    
+    {
+    cout << "The output is: " << a[i] << endl;
+    }
+    
+    cout << "The array size is: "<< num + 1;
+    
+        
+_getch();
+
+return 0;
+}
